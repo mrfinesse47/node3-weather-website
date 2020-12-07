d7da3f9556240b3f74311f6156604f8e0f7c2ad7
Kevin Mason <kevinmason@MacBook-Pro.local>
2020-12-06 20:46:43 -0800

    second commit

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,6 @@ const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
 
-// messageOne.textContent = "From Javascript";
-
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   messageOne.textContent = "loading";
